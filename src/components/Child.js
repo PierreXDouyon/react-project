5ad8d437ef11ade5aba7051c7bbf6e5b142f9966
PierreXDouyon <2295472@edu.vaniercollege.qc.ca>
2023-09-05 16:34:25 -0400

    Update

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,11 +1,14 @@
 function Child (props) {
   return (
-    <div>
-      {/* <h3>Child Component</h3>
-      <p>{props.name}</p> */}
-      {props.name ? <p>{props.name}</p> : <p>No name Passed</p>}
+
+      // {<h3>Child Component</h3>
+      // <p>{props.name}</p> }
+<div className={props.className ? props.className : "default-class"}>
+      {/* {props.name ? <h3>{props.name}</h3> : <p>No name Passed</p>} */}
+      {props.obj ?  <h3>{props.obj.id} - {props.obj.name}</h3>  :  <p>No object Passed</p> }
+      {props.obj ? ( <> <h3> Object: {props.obj.id} - {props.obj.name}</h3> <p>{props.obj.items}</p> </> ) : ( <p>No object Passed</p>)}
     </div>
-  )
+  );
 }
 
 export default Child;
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,13 +1,24 @@
 import Child from "./Child"
 const testArray = ["one", "two", "three"];
+const testArrayOfObjects = [
+  {id: 1, name: "one", items: [1,2,3]},
+  {id: 2, name: "two", items: [4,5,6]},
+  {id: 3, name: "three", items: [7,8,9]},
+  {id: 4, name: "four", items: [10,11,12]},
+  {id: 5, name: "five", items: [13,14,15]},
+  {id: 6, name: "six", items: [16,17,18]},
+];
 
 function Parent() {
   return (
     <div>
-      <h2>Parent</h2>
-      <Child />
+      <h2>Parent Component</h2>
+      <Child className="props-class"/>
       {testArray.map((item, index) => {
-        return <Child key={index}>{item}</Child>
+        return <Child key={index}name={item}/>
+      })}
+      {testArrayOfObjects.map((item, index) => {
+        return <Child key={item.id}obj={item}/>
       })}
     </div>
   );
